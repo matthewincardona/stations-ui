2c490ff95f87a58390acbcca83047c00fe5e5e05
Matthew Incardona <doublethink.inc@gmail.com>
2025-12-25 12:51:05 -0500

    feat: add navbar

---
+++ app/components/navbar.tsx
@@ -0,0 +1,22 @@
+export default function Navbar() {
+  return (
+    <nav className="max-w-full  flex justify-center">
+      <div className="flex justify-between bg-[#4C73F2] h-12 max-w-7xl grow items-center rounded-xl p-8 mt-8">
+        <div className="flex gap-4">
+          <p>
+            <b>Stations</b>
+          </p>
+          <p>Entry Level Jobs For Tech People</p>
+        </div>
+        <ul className="flex gap-4">
+          <li className="underline">
+            <a href="">Find Jobs</a>
+          </li>
+          <li className="underline">
+            <a href="">Test Your Resume</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
