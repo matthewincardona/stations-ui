0e121a437ae0ae21d7425df2c65150f2ca3b086b
Matthew Incardona <doublethink.inc@gmail.com>
2025-12-26 21:09:39 -0500

    feat: add link to home

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,9 +3,11 @@ export default function Navbar() {
     <nav className="max-w-full  flex justify-center text-white">
       <div className="flex justify-between bg-[#4C73F2] h-12 max-w-400 grow items-center rounded-xl p-8">
         <div className="flex gap-4">
-          <p>
-            <b>Stations</b>
-          </p>
+          <a href="/">
+            <p>
+              <b>Stations</b>
+            </p>
+          </a>
           <p>Entry Level Jobs For Tech People</p>
         </div>
         <ul className="flex gap-4">
