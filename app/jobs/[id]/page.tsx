b185d51e2f8bfc2a49ae88a2f2be7ca4a2fde985
Matthew Incardona <doublethink.inc@gmail.com>
2026-01-04 11:36:43 -0500

    ui: remove back button

--- app/jobs/[id]/page.tsx
+++ app/jobs/[id]/page.tsx
@@ -1,22 +1,11 @@
 "use client";
 
-import JobDetail from "../../components/JobDetail";
-import { useParams, useRouter } from "next/navigation";
+import Navbar from "../../components/Navbar";
 
 export default function JobPage() {
-  const { id } = useParams();
-  const router = useRouter();
-
   return (
-    <div className="max-w-3xl mx-auto p-6">
-      <button
-        className="mb-6 py-2 px-5 bg-[#F5F5F5] rounded-full"
-        onClick={() => router.back()}
-      >
-        Back
-      </button>
-
-      <JobDetail id={id as string} />
+    <div>
+      <Navbar />
     </div>
   );
 }
